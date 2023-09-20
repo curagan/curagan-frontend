95d9c965fb9d494184cf2d9f14322616895648ba
anggr <anggorookky@gmail.com>
2023-09-21 07:44:51 +0800

    docs: Update Commit Message

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'subject-case': [2, 'always', ['lower-case', 'upper-case']],
+    'subject-case': [
+      2,
+      'always',
+      ['lower-case', 'upper-case', 'start-case', 'camel-case'],
+    ],
   },
 };
