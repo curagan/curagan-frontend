495aa181333e6b8034e2329feb3564af72b4dfcd
anggr <anggorookky@gmail.com>
2023-09-25 13:14:08 +0800

    style: fix single quotes and add semicolons

---
+++ pages/jadwal/index.tsx
@@ -0,0 +1,22 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+import Button from '../../components/Button';
+
+const Index: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>curagan</title>
+        <meta name="description" content="curagan" />
+      </Head>
+      <div className="flex flex-col justify-center items-center h-screen space-y-4">
+        <h1 className="text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+          Hello World
+        </h1>
+        <Button text="Click Me!" onClick={() => alert('Button Clicked!')} />
+      </div>
+    </>
+  );
+};
+
+export default Index;
