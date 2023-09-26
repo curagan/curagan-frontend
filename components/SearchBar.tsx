da7d991f7dde0b9451b730032cf5f0407b94b4c8
Derian Melvin <derianmelvin88@gmail.com>
2023-09-26 15:21:10 +0700

    style: update homepage
    - update SearchBar styling
    - update Footer styling

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+import imgSearch from '@/public/icons/search.png';
+
 export const SearchBar = () => {
   return (
     <form className="w-full">
@@ -5,7 +8,13 @@ export const SearchBar = () => {
         Cari dokter atau spesialisasi
       </label>
       <div className="relative flex items-center">
-        <div className="absolute h-6 w-6 rounded-full bg-slate-600 translate-x-3"></div>
+        <div className="absolute h-6 w-6 rounded-full translate-x-3">
+          <Image
+            src={imgSearch}
+            alt="search bar"
+            className="w-full object-contain"
+          />
+        </div>
         <input
           type="text"
           name="searchBar"
