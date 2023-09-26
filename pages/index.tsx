f9d8b5567de25c49d2527fdac167f81a3b0a4269
Derian Melvin <derianmelvin88@gmail.com>
2023-09-26 15:37:37 +0700

    fix: redirect user from '/' to '/beranda'
    - remove default content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,21 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
-import Button from '../components/Button';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const Index: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/beranda');
+  }, []);
+
   return (
     <>
       <Head>
-        <title>curagan</title>
+        <title>Curagan</title>
         <meta name="description" content="curagan" />
       </Head>
-      <div className="flex flex-col justify-center items-center h-screen space-y-4">
-        <h1 className="text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-          Hello World
-        </h1>
-        <Button text="Click Me!" onClick={() => alert('Button Clicked!')} />
-      </div>
     </>
   );
 };
