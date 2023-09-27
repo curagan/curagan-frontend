5bfe902460ca00058e9cae976d0ebc8710f5e3a7
Derian Melvin <derianmelvin88@gmail.com>
2023-09-27 13:35:29 +0700

    fix: adjust api links for patient and doctor

--- lib/ApiLinks.ts
+++ lib/ApiLinks.ts
@@ -1,4 +1,6 @@
-export const API_REGISTER =
+export const API_REGISTER_PATIENT =
   'https://curagan-api.nikenhpsr.site/patient/auth/register';
-export const API_LOGIN =
+export const API_LOGIN_PATIENT =
   'https://curagan-api.nikenhpsr.site/patient/auth/login';
+export const API_LOGIN_DOCTOR =
+  'https://curagan-api.nikenhpsr.site/doctor/auth/login';
