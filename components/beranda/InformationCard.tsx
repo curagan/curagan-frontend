7dc0a8315834ca5cc0bca9eb017a53ee568f0a6c
Derian Melvin <derianmelvin88@gmail.com>
2023-09-28 19:24:06 +0700

    feat: update placeholder icons
    - import icons to public\icons
    - update component DoctorList
    - update component InformationCard
    - update component DoctorCard

--- components/beranda/InformationCard.tsx
+++ components/beranda/InformationCard.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+import imgCheck from '@/public/icons/check.png';
+
 export const InformationCard = () => {
   return (
     <div className="w-full flex flex-col items-center gap-4 p-3 rounded-md bg-slate-100">
@@ -5,15 +8,21 @@ export const InformationCard = () => {
         Buat Janji Mudah & Bebas Antre
       </h2>
       <div className="w-5/6 flex items-center gap-3">
-        <div className="w-14 h-14 min-w-fit rounded-full bg-gray-600"></div>
+        <div className="w-14 h-14">
+          <Image src={imgCheck} alt="benefit 1" className="object-contain" />
+        </div>
         <p className="text-sm">Tersedia dokter dari berbagai wilayah</p>
       </div>
       <div className="w-5/6 flex items-center gap-3">
-        <div className="w-14 h-14 min-w-fit rounded-full bg-gray-600"></div>
+        <div className="w-14 h-14">
+          <Image src={imgCheck} alt="benefit 2" className="object-contain" />
+        </div>
         <p className="text-sm">Cepat - tanpa antre & daftar ulang</p>
       </div>
       <div className="w-5/6 flex items-center gap-3">
-        <div className="w-14 h-14 min-w-fit rounded-full bg-gray-600"></div>
+        <div className="w-14 h-14">
+          <Image src={imgCheck} alt="benefit 3" className="object-contain" />
+        </div>
         <p className="text-sm">Tersedia ragam jadwal sesuai kebutuhanmu</p>
       </div>
     </div>
