795da2cb9ca9468ccd691c96134cc413013185ab
anggr <anggorookky@gmail.com>
2023-09-29 13:59:32 +0800

    feat: add month and year selection functionality

--- components/jadwal/CalendarComponent.tsx
+++ components/jadwal/CalendarComponent.tsx
@@ -8,6 +8,9 @@ import {
   endOfMonth,
   eachDayOfInterval,
   getDaysInMonth,
+  getDay,
+  addDays,
+  isSameMonth,
 } from 'date-fns';
 import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -31,12 +34,15 @@ const validationSchema = yup.object().shape({
 type CalendarComponentProps = {
   schedule: Schedule[];
 };
-
 const CalendarComponent: React.FC<CalendarComponentProps> = ({ schedule }) => {
   const [currentDate, setCurrentDate] = useState(new Date());
   const [calendarDays, setCalendarDays] = useState<Date[]>([]);
   const [filteredSchedules, setFilteredSchedules] = useState<Schedule[]>([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedMonth, setSelectedMonth] = useState<number | null>(null);
+  const [dayOptions, setDayOptions] = useState<number[]>([]);
+  const [selectedYear, setSelectedYear] = useState<number | null>(null);
+  const [refreshOptions, setRefreshOptions] = useState(false);
   const { control, handleSubmit, errors } = useForm<Schedule>({
     resolver: yupResolver(validationSchema),
   }) as any;
@@ -67,9 +73,19 @@ const CalendarComponent: React.FC<CalendarComponentProps> = ({ schedule }) => {
   useEffect(() => {
     fetchDataFromAPI();
   }, []);
+  const openModal = () => {
+    setIsModalOpen(true);
 
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+    setSelectedMonth(new Date().getMonth() + 1);
+    setSelectedYear(new Date().getFullYear());
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+
+    setSelectedMonth(null);
+    setSelectedYear(null);
+  };
 
   const handleFormSubmit: SubmitHandler<Schedule> = async (data: Schedule) => {
     const doctorId = localStorage.getItem('doctorId');
@@ -87,7 +103,6 @@ const CalendarComponent: React.FC<CalendarComponentProps> = ({ schedule }) => {
       },
     };
 
-    // Combining existing schedules with new one
     const existingSchedules = [...(filteredSchedules ?? [])];
     existingSchedules.push(data);
 
@@ -109,6 +124,22 @@ const CalendarComponent: React.FC<CalendarComponentProps> = ({ schedule }) => {
   };
 
   useEffect(() => {
+    const getDaysInSelectedMonth = () => {
+      if (selectedMonth !== null && selectedYear !== null) {
+        return getDaysInMonth(new Date(selectedYear, selectedMonth - 1));
+      }
+      return 31;
+    };
+
+    const newDayOptions = Array.from(
+      { length: getDaysInSelectedMonth() },
+      (_, i) => i + 1,
+    );
+
+    setDayOptions(newDayOptions);
+  }, [selectedMonth, selectedYear]);
+
+  useEffect(() => {
     const start = startOfMonth(currentDate);
     const end = endOfMonth(currentDate);
     setCalendarDays(eachDayOfInterval({ start, end }));
@@ -126,6 +157,21 @@ const CalendarComponent: React.FC<CalendarComponentProps> = ({ schedule }) => {
     format(new Date(2022, i, 1), 'MMMM'),
   );
 
+  useEffect(() => {
+    const start = startOfMonth(currentDate);
+    const end = endOfMonth(currentDate);
+
+    const firstDayOfMonth = getDay(start);
+
+    const placeholders = Array.from({ length: firstDayOfMonth }, (_, i) =>
+      addDays(start, i - firstDayOfMonth),
+    );
+
+    const daysOfMonth = eachDayOfInterval({ start, end });
+
+    setCalendarDays([...placeholders, ...daysOfMonth]);
+  }, [currentDate]);
+
   return (
     <div>
       <div className="flex items-center justify-between mb-5">
@@ -134,7 +180,15 @@ const CalendarComponent: React.FC<CalendarComponentProps> = ({ schedule }) => {
         <button onClick={nextMonth}>&gt;</button>
         <button onClick={openModal}>+ tambah jadwal</button>
       </div>
-
+      <div className="grid grid-cols-7 gap-4">
+        <div>Minggu</div>
+        <div>Senin</div>
+        <div>Selasa</div>
+        <div>Rabu</div>
+        <div>Kamis</div>
+        <div>Jumat</div>
+        <div>Sabtu</div>
+      </div>
       {isModalOpen && (
         <div
           className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex justify-center items-center"
@@ -150,32 +204,52 @@ const CalendarComponent: React.FC<CalendarComponentProps> = ({ schedule }) => {
             >
               &times;
             </span>
-            <form onSubmit={handleSubmit(handleFormSubmit)}>
-              <label>
-                Date:
+            <form
+              onSubmit={handleSubmit(handleFormSubmit)}
+              className="space-y-4"
+            >
+              <div>
+                <label className="block text-sm font-medium text-gray-600">
+                  Date
+                </label>
                 <Controller
                   name="date"
                   control={control}
                   defaultValue=""
                   render={({ field }) => (
-                    <input
+                    <select
                       {...field}
-                      type="text"
-                      placeholder="DD"
-                      className="your-input-class"
-                    />
+                      className="mt-1 p-2 w-full border rounded-md"
+                    >
+                      {dayOptions.map((date) => (
+                        <option key={date} value={date}>
+                          {date}
+                        </option>
+                      ))}
+                    </select>
                   )}
                 />
-                {errors?.date && <p>Please select a day.</p>}{' '}
-              </label>
-              <label>
-                Month:
+                {errors?.date && (
+                  <p className="text-red-600">Please select a day.</p>
+                )}
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-600">
+                  Month
+                </label>
                 <Controller
                   name="month"
                   control={control}
                   defaultValue=""
                   render={({ field }) => (
-                    <select {...field}>
+                    <select
+                      {...field}
+                      className="mt-1 p-2 w-full border rounded-md"
+                      onChange={(e) => {
+                        field.onChange(e);
+                        setSelectedMonth(Number(e.target.value));
+                      }}
+                    >
                       {monthNames.map((month, index) => (
                         <option key={index} value={index + 1}>
                           {month}
@@ -184,16 +258,27 @@ const CalendarComponent: React.FC<CalendarComponentProps> = ({ schedule }) => {
                     </select>
                   )}
                 />
-                {errors?.month && <p>Please select a month.</p>}
-              </label>
-              <label>
-                Year:
+                {errors?.month && (
+                  <p className="text-red-600">Please select a month.</p>
+                )}
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-600">
+                  Year
+                </label>
                 <Controller
                   name="year"
                   control={control}
                   defaultValue=""
                   render={({ field }) => (
-                    <select {...field}>
+                    <select
+                      {...field}
+                      className="mt-1 p-2 w-full border rounded-md"
+                      onChange={(e) => {
+                        field.onChange(e);
+                        setSelectedYear(Number(e.target.value));
+                      }}
+                    >
                       {Array.from(
                         { length: 11 },
                         (_, i) => new Date().getFullYear() + i,
@@ -205,16 +290,23 @@ const CalendarComponent: React.FC<CalendarComponentProps> = ({ schedule }) => {
                     </select>
                   )}
                 />
-                {errors?.year && <p>Please select a year.</p>}
-              </label>
-              <label>
-                Time:
+                {errors?.year && (
+                  <p className="text-red-600">Please select a year.</p>
+                )}
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-600">
+                  Time
+                </label>
                 <Controller
                   name="time"
                   control={control}
                   defaultValue=""
                   render={({ field }) => (
-                    <select {...field}>
+                    <select
+                      {...field}
+                      className="mt-1 p-2 w-full border rounded-md"
+                    >
                       {timeSlots.map((time, index) => (
                         <option key={index} value={time}>
                           {time}
@@ -223,10 +315,18 @@ const CalendarComponent: React.FC<CalendarComponentProps> = ({ schedule }) => {
                     </select>
                   )}
                 />
-                {errors?.time && <p>Please select a time.</p>}
-              </label>
-              {errors?.time && <p>Please select a time.</p>}{' '}
-              <button type="submit">TAMBAH JADWAL</button>
+                {errors?.time && (
+                  <p className="text-red-600">Please select a time.</p>
+                )}
+              </div>
+              <div className="flex justify-end">
+                <button
+                  type="submit"
+                  className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+                >
+                  TAMBAH JADWAL
+                </button>
+              </div>
             </form>
           </div>
         </div>
@@ -243,10 +343,14 @@ const CalendarComponent: React.FC<CalendarComponentProps> = ({ schedule }) => {
               )
             : false;
 
+          const isCurrentMonth = isSameMonth(day, currentDate); // Add this line
+
           return (
             <div
               key={index}
-              className={`p-4 border ${isScheduled ? 'bg-blue-200' : ''}`}
+              className={`p-4 border ${isScheduled ? 'bg-blue-200' : ''} ${
+                !isCurrentMonth ? 'bg-gray-300' : ''
+              }`} // Modify this line
             >
               {day.getDate()}
             </div>
