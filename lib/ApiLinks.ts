3b88065a6abc3404adc761611dae85b9dfd517bc
Derian Melvin <derianmelvin88@gmail.com>
2023-09-29 19:03:26 +0700

    fix: update 'riwayat' page

--- lib/ApiLinks.ts
+++ lib/ApiLinks.ts
@@ -4,8 +4,9 @@ export const API_LOGIN_PATIENT =
   'https://curagan-api.nikenhpsr.site/patient/auth/login';
 export const API_LOGIN_DOCTOR =
   'https://curagan-api.nikenhpsr.site/doctor/auth/login';
-
 export const API_PATIENT = 'https://curagan-api.nikenhpsr.site/patient';
 export const API_DOCTOR = 'https://curagan-api.nikenhpsr.site/doctor';
 export const API_APPOINTMENT =
   'https://curagan-api.nikenhpsr.site/appointments';
+export const API_MY_APPOINTMENT =
+  'https://curagan-api.nikenhpsr.site/appointments/my-appointments';
