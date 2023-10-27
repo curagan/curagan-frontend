e79a4d5bacce3f5b7963433e81605ebbee719af6
yankyhermawan <yankyhermawan42@gmail.com>
2023-10-28 01:05:48 +0700

    Finish Accept/Reject Feature

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -6,149 +6,163 @@ import useSWR from "swr";
 import { boolean } from "yup";
 
 interface Appointment {
-  appointmentId: string;
-  patientID: string;
-  datetime: Date;
-  doctorID: string;
-  message: string | null;
-  status: string;
-  patient: Patient;
-  doctor: Doctor;
+	appointmentId: string;
+	patientID: string;
+	datetime: Date;
+	doctorID: string;
+	message: string | null;
+	status: string;
+	patient: Patient;
+	doctor: Doctor;
 }
 
 interface Patient {
-  id: string;
-  email: string;
-  name: string;
-  imageURL: string;
+	id: string;
+	email: string;
+	name: string;
+	imageURL: string;
 }
 
 interface Doctor {
-  id: string;
-  email: string;
-  specialization: string;
-  name: string;
-  imageURL: string;
-  location: string;
-  hospital: string;
-  schedule: string;
+	id: string;
+	email: string;
+	specialization: string;
+	name: string;
+	imageURL: string;
+	location: string;
+	hospital: string;
+	schedule: string;
 }
 export default function RiwayatID() {
-  const router = useRouter();
-  const initState = {
-    appointmentId: "",
-    patientID: "",
-    datetime: new Date(),
-    doctorID: "",
-    message: null,
-    status: "",
-    patient: {
-      id: "",
-      email: "",
-      name: "",
-      imageURL: "",
-    },
-    doctor: {
-      id: "",
-      email: "",
-      specialization: "",
-      name: "",
-      imageURL: "",
-      location: "",
-      hospital: "",
-      schedule: "",
-    },
-  };
-  const { id } = router.query;
-  const [appointmentData, setAppointmentData] =
-    useState<Appointment>(initState);
-  const [patientName, setPatientName] = useState("");
-  const [doctorName, setDoctorName] = useState("");
-  const [isRejected, setIsRejected] = useState(false);
-  const [message, setMessage] = useState("");
-  useEffect(() => {
-    const data = async () => {
-      const response = await fetch(`${API_APPOINTMENT}/${id}`);
-      const data = await response.json();
-      setAppointmentData(await data);
-      setPatientName(await data.patient.name);
-      setDoctorName(await data.doctor.name);
-    };
-    data();
-  }, [id]);
+	const router = useRouter();
+	const initState = {
+		appointmentId: "",
+		patientID: "",
+		datetime: new Date(),
+		doctorID: "",
+		message: null,
+		status: "",
+		patient: {
+			id: "",
+			email: "",
+			name: "",
+			imageURL: "",
+		},
+		doctor: {
+			id: "",
+			email: "",
+			specialization: "",
+			name: "",
+			imageURL: "",
+			location: "",
+			hospital: "",
+			schedule: "",
+		},
+	};
+	const { id } = router.query;
+	const [appointmentData, setAppointmentData] =
+		useState<Appointment>(initState);
+	const [patientName, setPatientName] = useState("");
+	const [doctorName, setDoctorName] = useState("");
+	const [isRejected, setIsRejected] = useState(false);
+	const [message, setMessage] = useState("");
+	useEffect(() => {
+		const data = async () => {
+			const response = await fetch(`${API_APPOINTMENT}/${id}`);
+			const data = await response.json();
+			setAppointmentData(await data);
+			setPatientName(await data.patient.name);
+			setDoctorName(await data.doctor.name);
+		};
+		data();
+	}, [id]);
 
-  const accept = async () => {
-    const response = await fetch(`${API_APPOINTMENT}/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-      body: JSON.stringify({
-        status: "Accepted",
-        message: "Accept Appointment",
-      }),
-    });
-  };
+	const accept = async () => {
+		await fetch(`${API_APPOINTMENT}/${id}`, {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${localStorage.getItem("token")}`,
+			},
+			body: JSON.stringify({
+				status: "Accepted",
+				message: "Accept Appointment",
+			}),
+		});
+	};
 
-  const reject = async (message: string) => {
-    const response = await fetch(`${API_APPOINTMENT}/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-      body: JSON.stringify({
-        status: "Rejected",
-        message: message,
-      }),
-    });
-  };
-  const rejectButton = () => {
-    setIsRejected(true);
-  };
-  const handleMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setMessage(e.target.value);
-  };
+	const reject = async () => {
+		console.log(message);
+		await fetch(`${API_APPOINTMENT}/${id}`, {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${localStorage.getItem("token")}`,
+			},
+			body: JSON.stringify({
+				status: "Rejected",
+				message: message,
+			}),
+		});
+	};
+	const rejectButton = () => {
+		setIsRejected(true);
+	};
+	const handleMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setMessage(e.target.value);
+	};
 
-  return (
-    <div>
-      <div className="p-4 rounded-lg border-black border-[1px] border-solid w-fit">
-        {appointmentData ? (
-          <div className="flex flex-col gap-2">
-            <span>
-              Patient Name:
-              {patientName}
-            </span>
-            <span>Doctor Name: {doctorName}</span>
-            <span>
-              Date & Time:
-              {String(appointmentData)}
-            </span>
-            <div className="flex flex-row justify-between">
-              <button
-                className="border-black border-[1px] border-solid w-fit rounded-md p-2 bg-green-300 hover:bg-green-500"
-                onClick={accept}
-              >
-                Accept
-              </button>
-              <button
-                className="border-black border-[1px] border-solid w-fit rounded-md p-2 bg-red-300 hover:bg-red-500"
-                onClick={rejectButton}
-              >
-                Reject
-              </button>
-            </div>
-            {isRejected ? (
-              <input type="text" onChange={handleMessage}></input>
-            ) : (
-              ""
-            )}
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="p-4 rounded-lg border-black border-[1px] border-solid w-fit">
+				{appointmentData ? (
+					<div className="flex flex-col gap-2">
+						<span>
+							Patient Name:
+							{patientName}
+						</span>
+						<span>Doctor Name: {doctorName}</span>
+						<span>
+							Date & Time:
+							{String(appointmentData)}
+						</span>
+						<div className="flex flex-row justify-between">
+							<button
+								className="border-black border-[1px] border-solid w-fit rounded-md p-2 bg-green-300 hover:bg-green-500"
+								onClick={accept}
+							>
+								Accept
+							</button>
+							<button
+								className="border-black border-[1px] border-solid w-fit rounded-md p-2 bg-red-300 hover:bg-red-500"
+								onClick={rejectButton}
+							>
+								Reject
+							</button>
+						</div>
+						{isRejected ? (
+							<>
+								<input
+									type="text"
+									onChange={handleMessage}
+									className="border-solid border-black border-[1px] p-2"
+									placeholder="Reject Message"
+								/>
+								<button
+									className="border-black border-[1px] border-solid w-full rounded-md p-2 bg-red-300 hover:bg-red-500"
+									onClick={() => reject()}
+								>
+									Submit
+								</button>
+							</>
+						) : (
+							""
+						)}
+					</div>
+				) : (
+					""
+				)}
+			</div>
+		</div>
+	);
 }
