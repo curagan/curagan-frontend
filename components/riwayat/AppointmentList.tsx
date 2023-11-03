0f71c168005ee7944b100f434fd760c823e15a89
Derian Melvin <derianmelvin88@gmail.com>
2023-11-03 19:58:37 +0700

    Revert "Create: Notification Feature"
    This reverts commit cf4a38b9fabad9ea5bc549107b7e1c31dce66b05.

--- components/riwayat/AppointmentList.tsx
+++ components/riwayat/AppointmentList.tsx
@@ -4,152 +4,152 @@ import imgDoctorAvatar from "@/public/icons/doctor-avatar.png";
 import { useState } from "react";
 
 interface IAppointmentList {
-	appointmentData: any;
-	doctorsData: any;
-	filterData: string;
+  appointmentData: any;
+  doctorsData: any;
+  filterData: string;
 }
 
 export const AppointmentList = ({
-	appointmentData,
-	doctorsData,
-	filterData,
+  appointmentData,
+  doctorsData,
+  filterData,
 }: IAppointmentList) => {
-	return (
-		<div className="w-full flex flex-col gap-3">
-			{appointmentData.map((appointment: any) => {
-				// Get doctor data
-				const doctor = doctorsData.find(
-					(doctor: any) => doctor.id == appointment.doctorID
-				);
+  return (
+    <div className="w-full flex flex-col gap-3">
+      {appointmentData.map((appointment: any) => {
+        // Get doctor data
+        const doctor = doctorsData.find(
+          (doctor: any) => doctor.id == appointment.doctorID
+        );
 
-				// Get date & time
-				const thisDate = new Date(appointment.datetime);
-				const getDate = thisDate.toLocaleDateString("id-ID", {
-					weekday: "long",
-					day: "numeric",
-					month: "long",
-					year: "numeric",
-				});
-				const getTime = thisDate.toLocaleTimeString("id-ID", {
-					hour: "2-digit",
-					minute: "2-digit",
-				});
+        // Get date & time
+        const thisDate = new Date(appointment.datetime);
+        const getDate = thisDate.toLocaleDateString("id-ID", {
+          weekday: "long",
+          day: "numeric",
+          month: "long",
+          year: "numeric",
+        });
+        const getTime = thisDate.toLocaleTimeString("id-ID", {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
 
-				if (filterData == "All") {
-					return (
-						<a
-							key={appointment.appointmentId}
-							className="w-full flex flex-col gap-2 p-2 border rounded-md bg-slate-200 cursor-pointer hover:bg-slate-400"
-							href={`/detail/${appointment.appointmentId}`}
-						>
-							<div className="w-full flex items-center justify-between gap-2 text-lg font-medium">
-								<span>{getDate}</span>
-								<span>{getTime}</span>
-							</div>
+        if (filterData == "All") {
+          return (
+            <a
+              key={appointment.appointmentId}
+              className="w-full flex flex-col gap-2 p-2 border rounded-md bg-slate-200 cursor-pointer hover:bg-slate-400"
+              href={`/riwayat/${appointment.appointmentId}`}
+            >
+              <div className="w-full flex items-center justify-between gap-2 text-lg font-medium">
+                <span>{getDate}</span>
+                <span>{getTime}</span>
+              </div>
 
-							<div className="w-full flex items-center justify-between gap-2">
-								<div className="flex items-center gap-1">
-									<div className="w-5 h-5 flex items-center justify-center rounded-md">
-										<Image
-											src={imgLocation}
-											alt="location"
-											className="object-contain"
-										/>
-									</div>
-									<span className="text-sm text-gray-600">
-										{`${doctor.hospital}, ${doctor.location}`}
-									</span>
-								</div>
-							</div>
+              <div className="w-full flex items-center justify-between gap-2">
+                <div className="flex items-center gap-1">
+                  <div className="w-5 h-5 flex items-center justify-center rounded-md">
+                    <Image
+                      src={imgLocation}
+                      alt="location"
+                      className="object-contain"
+                    />
+                  </div>
+                  <span className="text-sm text-gray-600">
+                    {`${doctor.hospital}, ${doctor.location}`}
+                  </span>
+                </div>
+              </div>
 
-							<div className="w-full flex items-center gap-2 ml-3 my-2">
-								<div className="w-14 h-14 flex items-center justify-center rounded-md border">
-									<Image
-										src={imgDoctorAvatar}
-										alt="doctor image"
-										className="object-contain"
-									/>
-								</div>
-								<span>{doctor.name}</span>
-							</div>
+              <div className="w-full flex items-center gap-2 ml-3 my-2">
+                <div className="w-14 h-14 flex items-center justify-center rounded-md border">
+                  <Image
+                    src={imgDoctorAvatar}
+                    alt="doctor image"
+                    className="object-contain"
+                  />
+                </div>
+                <span>{doctor.name}</span>
+              </div>
 
-							<div className="w-full flex items-center justify-end gap-2">
-								<span
-									className={`w-fit rounded-md font-medium ${
-										appointment.status == "Pending" ||
-										appointment.status == "Submitted"
-											? "text-slate-900"
-											: appointment.status == "Accepted"
-											? "text-green-500"
-											: appointment.status == "Rejected"
-											? "text-red-500"
-											: "text-gray-500"
-									}`}
-								>
-									{appointment.status}
-								</span>
-							</div>
-						</a>
-					);
-				} else {
-					return appointment.status == filterData ? (
-						<div
-							key={appointment.appointmentId}
-							className="w-full flex flex-col gap-2 p-2 border rounded-md bg-slate-200"
-						>
-							<div className="w-full flex items-center justify-between gap-2 text-lg font-medium">
-								<span>{getDate}</span>
-								<span>{getTime}</span>
-							</div>
+              <div className="w-full flex items-center justify-end gap-2">
+                <span
+                  className={`w-fit rounded-md font-medium ${
+                    appointment.status == "Pending" ||
+                    appointment.status == "Submitted"
+                      ? "text-slate-900"
+                      : appointment.status == "Accepted"
+                      ? "text-green-500"
+                      : appointment.status == "Rejected"
+                      ? "text-red-500"
+                      : "text-gray-500"
+                  }`}
+                >
+                  {appointment.status}
+                </span>
+              </div>
+            </a>
+          );
+        } else {
+          return appointment.status == filterData ? (
+            <div
+              key={appointment.appointmentId}
+              className="w-full flex flex-col gap-2 p-2 border rounded-md bg-slate-200"
+            >
+              <div className="w-full flex items-center justify-between gap-2 text-lg font-medium">
+                <span>{getDate}</span>
+                <span>{getTime}</span>
+              </div>
 
-							<div className="w-full flex items-center justify-between gap-2">
-								<div className="flex items-center gap-1">
-									<div className="w-5 h-5 flex items-center justify-center rounded-md">
-										<Image
-											src={imgLocation}
-											alt="location"
-											className="object-contain"
-										/>
-									</div>
-									<span className="text-sm text-gray-600">
-										{`${doctor.hospital}, ${doctor.location}`}
-									</span>
-								</div>
-							</div>
+              <div className="w-full flex items-center justify-between gap-2">
+                <div className="flex items-center gap-1">
+                  <div className="w-5 h-5 flex items-center justify-center rounded-md">
+                    <Image
+                      src={imgLocation}
+                      alt="location"
+                      className="object-contain"
+                    />
+                  </div>
+                  <span className="text-sm text-gray-600">
+                    {`${doctor.hospital}, ${doctor.location}`}
+                  </span>
+                </div>
+              </div>
 
-							<div className="w-full flex items-center gap-2 ml-3 my-2">
-								<div className="w-14 h-14 flex items-center justify-center rounded-md border">
-									<Image
-										src={imgDoctorAvatar}
-										alt="doctor image"
-										className="object-contain"
-									/>
-								</div>
-								<span>{doctor.name}</span>
-							</div>
+              <div className="w-full flex items-center gap-2 ml-3 my-2">
+                <div className="w-14 h-14 flex items-center justify-center rounded-md border">
+                  <Image
+                    src={imgDoctorAvatar}
+                    alt="doctor image"
+                    className="object-contain"
+                  />
+                </div>
+                <span>{doctor.name}</span>
+              </div>
 
-							<div className="w-full flex items-center justify-end gap-2">
-								<span
-									className={`w-fit px-2 py-1 rounded-md font-medium ${
-										appointment.status == "Pending" ||
-										appointment.status == "Submitted"
-											? "text-slate-900"
-											: appointment.status == "Accepted"
-											? "text-green-500"
-											: appointment.status == "Rejected"
-											? "text-red-500"
-											: "text-gray-500"
-									}`}
-								>
-									{appointment.status}
-								</span>
-							</div>
-						</div>
-					) : (
-						""
-					);
-				}
-			})}
-		</div>
-	);
+              <div className="w-full flex items-center justify-end gap-2">
+                <span
+                  className={`w-fit px-2 py-1 rounded-md font-medium ${
+                    appointment.status == "Pending" ||
+                    appointment.status == "Submitted"
+                      ? "text-slate-900"
+                      : appointment.status == "Accepted"
+                      ? "text-green-500"
+                      : appointment.status == "Rejected"
+                      ? "text-red-500"
+                      : "text-gray-500"
+                  }`}
+                >
+                  {appointment.status}
+                </span>
+              </div>
+            </div>
+          ) : (
+            ""
+          );
+        }
+      })}
+    </div>
+  );
 };
--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,6 +1,9 @@
-import { API_APPOINTMENT } from "@/lib/ApiLinks";
-import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import { API_APPOINTMENT } from "@/lib/ApiLinks";
+import { useState, useEffect, TextareaHTMLAttributes } from "react";
+import axios from "axios";
+import useSWR from "swr";
+import { boolean } from "yup";
 
 interface Appointment {
 	appointmentId: string;
@@ -32,20 +35,50 @@ interface Doctor {
 }
 export default function RiwayatID() {
 	const router = useRouter();
-	const [appointmentData, setAppointmentData] = useState<Appointment[]>([]);
+	const initState = {
+		appointmentId: "",
+		patientID: "",
+		datetime: new Date(),
+		doctorID: "",
+		message: null,
+		status: "",
+		patient: {
+			id: "",
+			email: "",
+			name: "",
+			imageURL: "",
+		},
+		doctor: {
+			id: "",
+			email: "",
+			specialization: "",
+			name: "",
+			imageURL: "",
+			location: "",
+			hospital: "",
+			schedule: "",
+		},
+	};
+	const { id } = router.query;
+	const [appointmentData, setAppointmentData] =
+		useState<Appointment>(initState);
+	const [patientName, setPatientName] = useState("");
+	const [doctorName, setDoctorName] = useState("");
 	const [isRejected, setIsRejected] = useState(false);
 	const [message, setMessage] = useState("");
 	useEffect(() => {
 		const data = async () => {
-			const response = await fetch(`${API_APPOINTMENT}/${router.query.id}`);
+			const response = await fetch(`${API_APPOINTMENT}/${id}`);
 			const data = await response.json();
-			setAppointmentData((prev) => [...prev, data]);
+			setAppointmentData(await data);
+			setPatientName(await data.patient.name);
+			setDoctorName(await data.doctor.name);
 		};
 		data();
-	}, [router.query.id]);
+	}, [id]);
 
 	const accept = async () => {
-		await fetch(`${API_APPOINTMENT}/${router.query.id}`, {
+		await fetch(`${API_APPOINTMENT}/${id}`, {
 			method: "PATCH",
 			headers: {
 				"Content-Type": "application/json",
@@ -60,7 +93,7 @@ export default function RiwayatID() {
 
 	const reject = async () => {
 		console.log(message);
-		await fetch(`${API_APPOINTMENT}/${router.query.id}`, {
+		await fetch(`${API_APPOINTMENT}/${id}`, {
 			method: "PATCH",
 			headers: {
 				"Content-Type": "application/json",
@@ -75,77 +108,60 @@ export default function RiwayatID() {
 	const rejectButton = () => {
 		setIsRejected(true);
 	};
-	const handleMessage = (event: React.ChangeEvent<HTMLSelectElement>) => {
-		setMessage(event.target.value);
+	const handleMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setMessage(e.target.value);
 	};
 
-	useEffect(() => {
-		const id = router.query.id;
-		console.log(id);
-	}, []);
-
 	return (
-		<div className="flex min-h-screen justify-center items-center">
-			<div className="p-4 rounded-lg border-black border-[1px] border-solid w-fit h-fit">
-				{appointmentData
-					? appointmentData.map((data) => {
-							return (
-								<div className="flex flex-col gap-2">
-									<span>
-										Patient Name:
-										{data.patient.name}
-									</span>
-									<span>Doctor Name: {data.doctor.name}</span>
-									<span>
-										Date & Time:
-										{String(data.datetime)}
-									</span>
-									<div className="flex flex-row justify-between">
-										<button
-											className="border-black border-[1px] border-solid w-fit rounded-md p-2 bg-green-300 hover:bg-green-500"
-											onClick={accept}
-										>
-											Accept
-										</button>
-										<button
-											className="border-black border-[1px] border-solid w-fit rounded-md p-2 bg-red-300 hover:bg-red-500"
-											onClick={rejectButton}
-										>
-											Reject
-										</button>
-									</div>
-									{isRejected ? (
-										<>
-											<select
-												className="border-black border-solid border-[1px] rounded-md p-2"
-												onChange={handleMessage}
-											>
-												<option value="">--Reason--</option>
-												<option value="Full booked at desired time">
-													Full booked at desired time
-												</option>
-												<option value="Doctor not on duty">
-													Doctor not on duty
-												</option>
-												<option value="Out of doctor expertise">
-													Out of doctor expertise
-												</option>
-												<option value="Unavailable">Unavailable</option>
-											</select>
-											<button
-												className="border-black border-[1px] border-solid w-full rounded-md p-2 bg-red-300 hover:bg-red-500"
-												onClick={() => reject()}
-											>
-												Submit
-											</button>
-										</>
-									) : (
-										""
-									)}
-								</div>
-							);
-					  })
-					: ""}
+		<div>
+			<div className="p-4 rounded-lg border-black border-[1px] border-solid w-fit">
+				{appointmentData ? (
+					<div className="flex flex-col gap-2">
+						<span>
+							Patient Name:
+							{patientName}
+						</span>
+						<span>Doctor Name: {doctorName}</span>
+						<span>
+							Date & Time:
+							{String(appointmentData)}
+						</span>
+						<div className="flex flex-row justify-between">
+							<button
+								className="border-black border-[1px] border-solid w-fit rounded-md p-2 bg-green-300 hover:bg-green-500"
+								onClick={accept}
+							>
+								Accept
+							</button>
+							<button
+								className="border-black border-[1px] border-solid w-fit rounded-md p-2 bg-red-300 hover:bg-red-500"
+								onClick={rejectButton}
+							>
+								Reject
+							</button>
+						</div>
+						{isRejected ? (
+							<>
+								<input
+									type="text"
+									onChange={handleMessage}
+									className="border-solid border-black border-[1px] p-2"
+									placeholder="Reject Message"
+								/>
+								<button
+									className="border-black border-[1px] border-solid w-full rounded-md p-2 bg-red-300 hover:bg-red-500"
+									onClick={() => reject()}
+								>
+									Submit
+								</button>
+							</>
+						) : (
+							""
+						)}
+					</div>
+				) : (
+					""
+				)}
 			</div>
 		</div>
 	);
