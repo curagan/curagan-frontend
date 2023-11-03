7ff8156d1522b771f53e41c8d170622c7206d6a8
Derian Melvin <derianmelvin88@gmail.com>
2023-11-03 20:06:26 +0700

    chore(ApiLinks): optimize code

--- lib/ApiLinks.ts
+++ lib/ApiLinks.ts
@@ -1,15 +1,10 @@
-export const API_REGISTER_PATIENT =
-  'https://curagan-api.nikenhpsr.site/patient/auth/register';
-export const API_LOGIN_PATIENT =
-  'https://curagan-api.nikenhpsr.site/patient/auth/login';
-export const API_LOGIN_DOCTOR =
-  'https://curagan-api.nikenhpsr.site/doctor/auth/login';
+const API = 'https://curagan-api.nikenhpsr.site';
 
-export const API_PATIENT = 'https://curagan-api.nikenhpsr.site/patient';
-export const API_DOCTOR = 'https://curagan-api.nikenhpsr.site/doctor';
-export const API_APPOINTMENT =
-  'https://curagan-api.nikenhpsr.site/appointments';
-export const API_MY_APPOINTMENT =
-  'https://curagan-api.nikenhpsr.site/appointments/my-appointments';
-export const API_APPOINTMENT_HISTORY =
-  'https://curagan-api.nikenhpsr.site/appointments/history';
+export const API_REGISTER_PATIENT = `${API}/patient/auth/register`;
+export const API_LOGIN_PATIENT = `${API}/patient/auth/login`;
+export const API_LOGIN_DOCTOR = `${API}/doctor/auth/login`;
+export const API_PATIENT = `${API}/patient`;
+export const API_DOCTOR = `${API}/doctor`;
+export const API_APPOINTMENT = `${API}/appointments`;
+export const API_MY_APPOINTMENT = `${API}/appointments/my-appointments`;
+export const API_APPOINTMENT_HISTORY = `${API}/appointments/history`;
