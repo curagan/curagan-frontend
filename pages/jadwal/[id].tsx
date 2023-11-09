cc3dd987b3c646893c6d40d2068b62eac2bb07b0
Derian Melvin <derianmelvin88@gmail.com>
2023-11-09 22:22:02 +0700

    refactor(pages\jadwal): adjust data fetching handler

--- pages/jadwal/[id].tsx
+++ pages/jadwal/[id].tsx
@@ -1,16 +1,19 @@
-import { GetServerSideProps } from 'next';
+import { NextPage } from 'next';
 import axios from 'axios';
 import CalendarComponent from '../../components/jadwal/CalendarComponent';
 import { useEffect, useState } from 'react';
 import { LayoutWrapper } from '@/components/layout/LayoutWrapper';
 import { API_DOCTOR } from '@/lib/ApiLinks';
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
+import { LoadingCard } from '@/components/LoadingCard';
 
-type Schedule = {
-  date: string;
-  month: string;
-  year: string;
-  time: string[];
-};
+// type Schedule = {
+//   date: string;
+//   month: string;
+//   year: string;
+//   time: string[];
+// };
 
 type Doctor = {
   id: string;
@@ -23,53 +26,52 @@ type Doctor = {
   schedule: string;
 };
 
-type JadwalProps = {
-  doctor: Doctor;
+type FetchDataType = {
+  data: Doctor;
+  isLoading: boolean;
 };
 
-const JadwalPage: React.FC<JadwalProps> = ({ doctor }) => {
-  const [parsedSchedule, setParsedSchedule] = useState<Schedule[]>([]);
+const JadwalPage: NextPage = () => {
+  const router = useRouter();
+  const userId = router.query.id;
 
+  // Handles SWR conditional fetching
+  const [shouldFetch, setShouldFetch] = useState(false);
+
+  // SWR
+  const fetcher = (url: string) =>
+    axios
+      .get(url, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      })
+      .then((res) => res.data);
+  const { data, isLoading }: FetchDataType = useSWR(
+    shouldFetch ? `${API_DOCTOR}/${userId}` : null,
+    fetcher,
+  );
+
+  // Fetch data after load
   useEffect(() => {
-    try {
-      const schedule: Schedule[] = JSON.parse(doctor.schedule);
-
-      setParsedSchedule(schedule);
-    } catch (err) {
-      console.error("Failed to parse doctor's schedule", err);
-    }
-  }, [doctor]);
+    setShouldFetch(true);
+  }, []);
 
   return (
     <LayoutWrapper>
-      <div>
-        <h1 className="font-bold">{doctor.name}</h1>
-        <p> Dokter {doctor.specialization}</p>
-        <p>{doctor.hospital}</p>
-        <CalendarComponent schedule={parsedSchedule} />
-      </div>
+      {isLoading || data == undefined ? (
+        <LoadingCard />
+      ) : (
+        <div>
+          <h1 className="font-bold">{data.name}</h1>
+          <p> Dokter {data.specialization}</p>
+          <p>{data.hospital}</p>
+          <CalendarComponent schedule={JSON.parse(data.schedule)} />
+        </div>
+      )}
     </LayoutWrapper>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const id = context.params?.id as string;
-  let doctor: Doctor;
-
-  try {
-    const res = await axios.get(`${API_DOCTOR}/${id}`);
-    doctor = res.data;
-  } catch (err) {
-    return {
-      notFound: true,
-    };
-  }
-
-  return {
-    props: {
-      doctor,
-    },
-  };
 };
 
 export default JadwalPage;
